ff644bb614c4373bb4bbd81c5025d0715c02cdbc
Pedro Nascimento <pedronascimento@enso-origins.com>
2022-05-05 16:27:11 +0100

    Breakpoints tests;

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entities/User.entity';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ){}
+
+  async getData(): Promise<{ message: string }> {
+
+    const tudo =  await this.userRepository.find();
+
+    await this.userRepository.insert({
+      username: null
+    })
+
     return { message: 'Welcome to api!' };
   }
 }
--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,7 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useEffect } from 'react';
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  useEffect(() => {
+    const a = 1;
+    const b = 2;
+    const c = 4;
+    const d = 4;
+  }, []);
+
   return (
     <>
       <NxWelcome title="web" />
